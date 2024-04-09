aa585f141653b3389f6e50eac031fdf6ecbdd860
Konstantin <kostya.safronov@gmail.com>
2024-04-09 14:35:51 +0300

    commit

--- project3/app/urls.py
+++ project3/app/urls.py
@@ -1,10 +1,15 @@
-
 from django.urls import path
 from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('hot/', views.hot, name='hot'),
+    path('login/', views.login, name='login'),
+    path('ask/', views.ask, name='ask'),
+    path('settings/', views.settings, name='settings'),
+    path('register/', views.register, name='register'),
+
+    path('questions/<int:question_id>', views.question, name='question')
 
 
 
--- project3/app/views.py
+++ project3/app/views.py
@@ -1,4 +1,4 @@
-
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,13 +7,34 @@ QUESTIONS = [
     {
         "title": f"Question {i}",
         "text": F"This is question number {i}"
-    } for i in range(10)
+    } for i in range(200)
 ]
 
 def index(request):
-    return render(request, "index.html", {"questions": QUESTIONS})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(QUESTIONS, 5)
+    page_obj = paginator.page(page_num)
+    return render(request, "index.html", {"questions": page_obj})
 
 
 def hot(request):
     questions = QUESTIONS[5:]
     return render(request, "hot.html", {"questions": questions})
+
+
+def login(request):
+    return render(request, "login.html")
+
+def ask(request):
+    return render(request, "ask.html")
+
+def settings(request):
+    return render(request, "settings.html")
+
+def register(request):
+    return render(request, "register.html")
+
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, "question_detail.html", {"question": item})
