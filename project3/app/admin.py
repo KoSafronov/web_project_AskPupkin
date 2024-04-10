18f04c422d5380a3b02a244140426d2cfb868d03
Konstantin <kostya.safronov@gmail.com>
2024-04-10 18:35:06 +0300

    commit

--- project3/app/admin.py
+++ project3/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from app import models
 
 # Register your models here.
+
+admin.site.register(models.User)
+admin.site.register(models.Profile)
+admin.site.register(models.Answer)
+admin.site.register(models.Tag)
+admin.site.register(models.Question)
+admin.site.register(models.Like)
+admin.site.register(models.link_like)
+admin.site.register(models.link_tag)
